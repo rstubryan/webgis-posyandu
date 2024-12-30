6de94ac47e31cea51a5c1e76d24b8258ad951571
restubryanr <restubumirr@gmail.com>
2024-12-30 12:40:36 +0700

    feat(webgis): add locations table to webgis page

--- src/app/webgis/page.tsx
+++ src/app/webgis/page.tsx
@@ -1,4 +1,13 @@
 import LeafletMap from "@/components/molecules/LeafletMap";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableCell,
+} from "@/components/ui/table";
+import locations from "@/data/locations.json";
 
 export default function Webgis() {
   return (
@@ -6,13 +15,35 @@ export default function Webgis() {
       <h1 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         WebGIS Posyandu di Desa Cisande
       </h1>
-      <p className="scroll-m-20 mt-4 text-lg">
+      <p className="scroll-m-20 my-4 text-lg">
         WebGIS Posyandu di Desa Cisande adalah sebuah aplikasi berbasis web yang
         memuat informasi mengenai posyandu di Desa Cisande. Aplikasi ini dibuat
         untuk memudahkan masyarakat dalam mencari informasi mengenai posyandu di
         Desa Cisande.
       </p>
       <LeafletMap />
+      <div className="mt-8">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Address</TableHead>
+              <TableHead>Latitude</TableHead>
+              <TableHead>Longitude</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {locations.map((location, index) => (
+              <TableRow key={index}>
+                <TableCell>{location.name}</TableCell>
+                <TableCell>{location.address}</TableCell>
+                <TableCell>{location.lat}</TableCell>
+                <TableCell>{location.lng}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </>
   );
 }
--- src/components/molecules/LeafletMap/index.tsx
+++ src/components/molecules/LeafletMap/index.tsx
@@ -5,6 +5,7 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import locations from "@/data/locations.json";
 
 const DefaultIcon = L.icon({
   iconUrl: icon as unknown as string,
@@ -13,45 +14,13 @@ const DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
-const locations = [
-  {
-    name: "Posyandu Rose",
-    address: "Desa Cisande RT 1, Kab. Sukabumi, Jawa Barat",
-    lat: -6.9012804,
-    lng: 106.860324,
-  },
-  {
-    name: "Posyandu Tulip",
-    address: "Desa Cisande RT 2, Kab. Sukabumi, Jawa Barat",
-    lat: -6.9012804,
-    lng: 106.85,
-  },
-  {
-    name: "Posyandu Kamboja",
-    address: "Desa Cisande RT 3, Kab. Sukabumi, Jawa Barat",
-    lat: -6.9012804,
-    lng: 106.84,
-  },
-  {
-    name: "Posyandu Melati",
-    address: "Desa Cisande RT 4, Kab. Sukabumi, Jawa Barat",
-    lat: -6.91,
-    lng: 106.85321,
-  },
-  {
-    name: "Posyandu Anggrek",
-    address: "Desa Cisande RT 5, Kab. Sukabumi, Jawa Barat",
-    lat: -6.91,
-    lng: 106.860324,
-  },
-];
-
 export default function LeafletMap() {
   return (
     <MapContainer
       center={[-6.9012804, 106.860324]}
       zoom={15}
       style={{ height: "50vh", width: "100%" }}
+      className={`rounded-lg border border-solid border-black/[.08] dark:border-white/[.145]`}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
